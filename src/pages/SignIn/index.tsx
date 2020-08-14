9cf83cf78f433713ad55532ac06e591657e719d7
JosueFS <josue.18rj@gmail.com>
2020-08-13 23:31:52 -0300

    N0306 - Input & Button ✅

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,15 +1,39 @@
 import React from 'react';
 import { Image } from 'react-native';
 
-import { Container, Title } from './styles';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
 
 import logoImg from '../../assets/logo.png';
+
+import { Container, Title } from './styles';
 
 const SignIn: React.FC = () => (
   <Container>
     <Image source={logoImg} />
 
     <Title>Faça seu logon</Title>
+
+    <Input
+      name="email"
+      icon="mail"
+      placeholderTextColor="#666360"
+      placeholder="E-mail"
+    />
+    <Input
+      name="password"
+      icon="lock"
+      placeholderTextColor="#666360"
+      placeholder="Senha"
+    />
+
+    <Button
+      onPress={() => {
+        console.log('Pressed!');
+      }}
+    >
+      Entrar
+    </Button>
   </Container>
 );
 
