5bf1514f0adf41aa622a821d69c3549b4cdaf877
JosueFS <josue.18rj@gmail.com>
2021-03-04 00:01:35 -0300

    new: Sucessful Appointment creation page

--- src/pages/AppointmentCreated/index.tsx
+++ src/pages/AppointmentCreated/index.tsx
@@ -1,10 +1,58 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useMemo, useCallback } from 'react';
+import Icon from 'react-native-vector-icons/Feather';
 
-const AppointmentCreated: React.FC = () => (
-  <View>
-    <Text>AppointmentCreated</Text>
-  </View>
-);
+import { useRoute, useNavigation } from '@react-navigation/native';
+import { format } from 'date-fns';
+import { ptBR } from 'date-fns/locale';
+
+import {
+  Container,
+  Title,
+  Description,
+  OkButton,
+  OkButtonText,
+} from './styles';
+
+interface RouteParams {
+  date: number;
+}
+
+const AppointmentCreated: React.FC = () => {
+  const route = useRoute();
+  const navigation = useNavigation();
+  const params = route.params as RouteParams;
+
+  const formattedDate = useMemo(
+    () =>
+      format(params.date, "EEEE', dia' dd 'de' MMMM 'de' yyyy 'às' HH:mm'h'", {
+        locale: ptBR,
+      }),
+    [params.date],
+  );
+
+  const handleOk = useCallback(() => {
+    navigation.reset({
+      index: 0,
+      routes: [
+        {
+          name: 'Dashboard',
+        },
+      ],
+    });
+  }, [navigation]);
+
+  return (
+    <Container>
+      <Icon name="check" size={80} color="#04d361" />
+
+      <Title>Agendamento concluído</Title>
+      <Description>{formattedDate}</Description>
+
+      <OkButton onPress={handleOk}>
+        <OkButtonText>Ok</OkButtonText>
+      </OkButton>
+    </Container>
+  );
+};
 
 export default AppointmentCreated;
